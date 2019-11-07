af5fd93618e97c905621bfed231ab042a408fd96
Ricardo Edenilson Gutierres Mauricio <42420059+Edenilson15@users.noreply.github.com>
2019-11-07 11:45:43 -0600

    Inicio de Proyecto

---
+++ app/build/generated/source/buildConfig/debug/com/eg/himnario/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.eg.himnario;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.eg.himnario";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
