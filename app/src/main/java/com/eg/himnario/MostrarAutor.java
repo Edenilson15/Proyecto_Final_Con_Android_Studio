395556fa58c1de0bc97902fede833d906fd64599
mauricio-19 <mauryuma59@gmail.com>
2019-11-13 22:45:08 -0600

    eh adicionado las javaclass y layout para la parte del inicio en la busqueda por autor

---
+++ app/src/main/java/com/eg/himnario/MostrarAutor.java
@@ -0,0 +1,14 @@
+package com.eg.himnario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarAutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_autor);
+    }
+}
---
+++ app/src/main/java/com/eg/himnario/NombreAutor.java
@@ -0,0 +1,14 @@
+package com.eg.himnario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NombreAutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nombre_autor);
+    }
+}
