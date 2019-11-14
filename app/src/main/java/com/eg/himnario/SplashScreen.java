04fcfe85bdbc141643adb2561062ccf87d28307f
BRENDACANALES29 <50680038+BRENDACANALES29@users.noreply.github.com>
2019-11-14 10:10:29 -0600

    cambios en la clase splash

--- app/src/main/java/com/eg/himnario/SplashScreen.java
+++ app/src/main/java/com/eg/himnario/SplashScreen.java
@@ -1,4 +1,21 @@
 package com.eg.himnario;
 
-public class SplashScreen {
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SplashScreen extends AppCompatActivity {
+    private TextView tv1,tv2,tv3,tv4,tv5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
 }
