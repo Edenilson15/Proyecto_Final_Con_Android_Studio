20bd9d03807b6152355dd94d0a94a4f2fa595c72
BRENDACANALES29 <50680038+BRENDACANALES29@users.noreply.github.com>
2019-11-14 10:59:38 -0600

    arregle la clase del splah

--- app/src/main/java/com/eg/himnario/SplashScreen.java
+++ app/src/main/java/com/eg/himnario/SplashScreen.java
@@ -1,21 +1,37 @@
 package com.eg.himnario;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class SplashScreen extends AppCompatActivity {
-    private TextView tv1,tv2,tv3,tv4,tv5;
+
+    private final int DURACION_SPLASH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, Inicio.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },DURACION_SPLASH);
     }
 }
